e5bc6fc50b221fc4ecf97586f1e88d9628560271
ASTAR <apo2073@outlook.com>
2026-02-01 22:33:53 +0900

    fix: login

--- app/api/login/confirm/route.ts
+++ app/api/login/confirm/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const code = searchParams.get("code");
 
